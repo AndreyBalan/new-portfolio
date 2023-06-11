2fe957e2123d8c5c6567e6cde6b71f9e12c95799
Andrii Balan <balan.andrey.93@gmail.com>
2023-06-11 15:54:26 +0300

    dev: skills done

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,6 +5,7 @@ import React, { useRef, useEffect } from 'react'
 import profilePic from "../../public/images/profile/developer-pic-2.jpg"
 import Image from 'next/image'
 import { useInView, useSpring, useMotionValue } from 'framer-motion'
+import Skills from '@/components/Skills'
 
 const AnimatedNumbers = ({value}) => {
 const ref = useRef(null);
@@ -89,6 +90,8 @@ const about = () => {
                             </div>
                         </div>
                 </div>
+
+                <Skills />
             </Layout>
         </main>
     </>
