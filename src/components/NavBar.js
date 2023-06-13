e6b27a222eb0fe48de2fd1feac1b5cdda97b3c71
Andrii Balan <balan.andrey.93@gmail.com>
2023-06-13 12:55:09 +0300

    dev: about and skills addaptive done

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -49,7 +49,7 @@ const CustomMobileLink = ({ href, title, className = "", toggle }) => {
 
             <span
                 className={`
-                h-[1px] inline-block w-0 bg-light dark:bg-dark absolute left-0 -bottom-0.5
+                h-[1px] inline-block w-0 bg-light absolute left-0 -bottom-0.5
                 group-hover:w-full transition-[width] ease duration-300 dark:bg-light
                 ${router.asPath === href ? "w-full" : "w-0"}
             `}>
--- src/pages/about.js
+++ src/pages/about.js
@@ -46,10 +46,10 @@ const about = () => {
                 <Layout className="pt-16">
                     <AnimatedText
                         text="Passion Fuels Purpose!"
-                        className="mb-16"
+                        className="mb-16 lg:!text-7xl sm:!text-6xl xs:!text-4xl sm:mb-8"
                     />
-                    <div className="grid w-full grid-cols-8 gap 16">
-                        <div className="col-span-3 flex flex-col items-start justify-start">
+                    <div className="grid w-full grid-cols-8 gap 16 sm:gap-8">
+                        <div className="col-span-3 flex flex-col items-start justify-start xl:col-span-4 md:order-2 md:col-span-8 md:mt-8">
                             <h2 className="mb-4 text-lg font-bold uppercase text-dark/75 dark:text-light/75">
                                 About Me
                             </h2>
@@ -76,7 +76,7 @@ const about = () => {
                                 to your next project.
                             </p>
                         </div>
-                        <div className="col-span-3 relative h-max rounded-2xl border-2 border-solid border-dark bg-light p-8 flex justify-center dark:bg-dark dark:border-light">
+                        <div className="col-span-3 relative h-max rounded-2xl border-2 border-solid border-dark bg-light p-8 flex justify-center dark:bg-dark dark:border-light xl:col-span-4 md:order-1 md:col-span-8">
                             <div className="absolute top-0 -right-3 -z-10 w-[102%] h-[102%] rounded-[2rem] bg-dark dark:bg-light"></div>
                             <Image
                                 src={profilePic}
@@ -88,30 +88,30 @@ const about = () => {
                             />
                         </div>
 
-                        <div className="col-span-2 flex flex-col items-end justify-between">
-                            <div className="flex flex-col items-end justify-center">
-                                <span className="inline-block text-7xl font-bold">
+                        <div className="col-span-2 flex flex-col items-end justify-between xl:col-span-8 xl:flex-row xl:mt-8 xl:items:center md:order-3">
+                            <div className="flex flex-col items-end justify-center xl:items-center">
+                                <span className="inline-block text-7xl font-bold md:text-6xl sm:text-5xl xs:text:4xl">
                                     <AnimatedNumbers value={50} /> +
                                 </span>
-                                <h2 className="text-xl font-medium capitalize text-dark/75 dark:text-light/75">
+                                <h2 className="text-xl font-medium capitalize text-dark/75 dark:text-light/75 xl:text-center md:text-lg sm:text-base xs:text-sm">
                                     Satisfied clients
                                 </h2>
                             </div>
 
-                            <div className="flex flex-col items-end justify-center">
-                                <span className="inline-block text-7xl font-bold">
+                            <div className="flex flex-col items-end justify-center xl:items-center">
+                                <span className="inline-block text-7xl font-bold md:text-6xl sm:text-5xl xs:text:4xl">
                                     <AnimatedNumbers value={40} /> +
                                 </span>
-                                <h2 className="text-xl font-medium capitalize text-dark/75 dark:text-light/75">
+                                <h2 className="text-xl font-medium capitalize text-dark/75 dark:text-light/75 xl:text-center md:text-lg sm:text-base xs:text-sm">
                                     projects completed
                                 </h2>
                             </div>
 
-                            <div className="flex flex-col items-end justify-center">
-                                <span className="inline-block text-7xl font-bold">
+                            <div className="flex flex-col items-end justify-center xl:items-center">
+                                <span className="inline-block text-7xl font-bold md:text-6xl sm:text-5xl xs:text:4xl">
                                     <AnimatedNumbers value={1} />+
                                 </span>
-                                <h2 className="text-xl font-medium capitalize text-dark/75 dark:text-light/75">
+                                <h2 className="text-xl font-medium capitalize text-dark/75 dark:text-light/75 xl:text-center md:text-lg sm:text-base xs:text-sm">
                                     years of experience
                                 </h2>
                             </div>
