0b3d9a44147bb6a0f5c754aab7069c066b040c58
Andrii Balan <balan.andrey.93@gmail.com>
2023-06-15 09:35:13 +0300

    fix: exit transition fix position

--- src/components/TransitionEffect.js
+++ src/components/TransitionEffect.js
@@ -5,13 +5,13 @@ const TransitionEffect = () => {
     return (
         <>
             <motion.div
-                className="fixed top-0 bottom-0 right-full w-screen h-screen z-30 bg-primary"
+                className="top-0 bottom-0 right-full w-screen h-screen z-30 bg-primary absolute"
                 initial={{ x: "100%", width: "100%" }}
                 animate={{ x: "0%", width: "0%" }}
                 exit={{ x: ["0%", "100%"], width: ["0%", "100%"] }}
                 transition={{ duration: 0.8, ease: "easeInOut" }}></motion.div>
             <motion.div
-                className="fixed top-0 bottom-0 right-full w-screen h-screen z-20 bg-light"
+                className="top-0 bottom-0 right-full w-screen h-screen z-20 bg-light absolute"
                 initial={{ x: "100%", width: "100%" }}
                 animate={{ x: "0%", width: "0%" }}
                 transition={{
@@ -20,7 +20,7 @@ const TransitionEffect = () => {
                     ease: "easeInOut",
                 }}></motion.div>
             <motion.div
-                className="fixed top-0 bottom-0 right-full w-screen h-screen z-10 bg-dark"
+                className="top-0 bottom-0 right-full w-screen h-screen z-10 bg-dark absolute"
                 initial={{ x: "100%", width: "100%" }}
                 animate={{ x: "0%", width: "0%" }}
                 transition={{
