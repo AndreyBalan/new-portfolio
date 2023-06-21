0e227ad09520e4af49f528cb73b11044df49bc17
Andrii Balan <balan.andrey.93@gmail.com>
2023-06-21 21:01:00 +0300

    fix: CLS fix

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -71,12 +71,12 @@ const NavBar = () => {
     return (
         <header className="w-full px-32 py-8 font-medium flex items-center justify-between dark:text-light relative z-10 lg:px-16 md:px-12 sm:px-8">
             <button
-                className="flex-col justify-center items-center hidden lg:flex"
+                className="flex-col justify-center items-center hidden lg:flex fixed"
                 onClick={handleClick}>
                 <span className="sr-only">Menu Button</span>
                 <span
                     className={`bg-dark dark:bg-light block transition-all duration-300 ease-out h-0.5 w-6 rounded-sm -translate-y-0.5 ${
-                        isOpen ? "rotate-45 translate-y-1" : "-translate-y-0.5"
+                        isOpen ? "rotate-45 translate-y-2" : "-translate-y-0.5"
                     }`}></span>
                 <span
                     className={`bg-dark dark:bg-light block transition-all duration-300 ease-out h-0.5 w-6 rounded-sm my-0.5 ${
@@ -171,7 +171,7 @@ const NavBar = () => {
                 <motion.div
                     initial={{ scale: 0, opacity: 0, x: "-50%", y: "-50%" }}
                     animate={{ scale: 1, opacity: 1 }}
-                    className="min-w-[70vw] flex flex-col justify-between z-30 items-center fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-dark/90 dark:bg-light/75 rounded-lg backdrop-blur-md py-32">
+                    className="min-w-[80vw] flex flex-col justify-between z-30 items-center fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-dark/90 dark:bg-light/75 rounded-lg backdrop-blur-md py-32">
                     <nav className="flex flex-col items-center justify-center">
                         <CustomMobileLink
                             href="/"
