689033e67287960a34a07533d8acadd8796dc2c5
Andrii Balan <balan.andrey.93@gmail.com>
2023-06-21 19:31:06 +0300

    fix: transition fix

--- src/components/TransitionEffect.js
+++ src/components/TransitionEffect.js
@@ -5,12 +5,15 @@ const TransitionEffect = () => {
     return (
         <>
             <motion.div
+                key="transition-effect-1"
                 className="fixed top-0 bottom-0 right-full w-screen h-screen z-30 bg-primary"
                 initial={{ x: "100%", width: "100%" }}
                 animate={{ x: "0%", width: "0%" }}
                 exit={{ x: ["0%", "100%"], width: ["0%", "100%"] }}
-                transition={{ duration: 0.8, ease: "easeInOut" }}></motion.div>
+                transition={{ duration: 0.8, ease: "easeInOut" }}
+                style={{ willChange: "transform, width" }}></motion.div>
             <motion.div
+                key="transition-effect-2"
                 className="fixed top-0 bottom-0 right-full w-screen h-screen z-20 bg-light"
                 initial={{ x: "100%", width: "100%" }}
                 animate={{ x: "0%", width: "0%" }}
@@ -18,8 +21,10 @@ const TransitionEffect = () => {
                     delay: 0.2,
                     duration: 0.8,
                     ease: "easeInOut",
-                }}></motion.div>
+                }}
+                style={{ willChange: "transform, width" }}></motion.div>
             <motion.div
+                key="transition-effect-3"
                 className="fixed top-0 bottom-0 right-full w-screen h-screen z-10 bg-dark"
                 initial={{ x: "100%", width: "100%" }}
                 animate={{ x: "0%", width: "0%" }}
@@ -27,7 +32,8 @@ const TransitionEffect = () => {
                     delay: 0.4,
                     duration: 0.8,
                     ease: "easeInOut",
-                }}></motion.div>
+                }}
+                style={{ willChange: "transform, width" }}></motion.div>
         </>
     );
 };
