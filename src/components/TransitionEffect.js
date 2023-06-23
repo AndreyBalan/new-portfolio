c764340c4e3122780a210b98fcb90a6f5717b4fe
Andrii Balan <balan.andrey.93@gmail.com>
2023-06-23 12:18:19 +0300

    fix: hireme fix

--- src/components/TransitionEffect.js
+++ src/components/TransitionEffect.js
@@ -1,18 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { motion } from "framer-motion";
 
 const TransitionEffect = () => {
+    const [initialLoad, setInitialLoad] = useState(true);
+
+    useEffect(() => {
+        // После отрисовки компонента устанавливаем initialLoad в false
+        setInitialLoad(false);
+    }, []);
+
     return (
         <>
             <motion.div
                 className="fixed top-0 bottom-0 right-full w-screen h-screen z-30 bg-primary"
-                initial={{ x: "100%", width: "100%" }}
+                initial={initialLoad ? false : { x: "100%", width: "100%" }}
                 animate={{ x: "0%", width: "0%" }}
                 exit={{ x: ["0%", "100%"], width: ["0%", "100%"] }}
                 transition={{ duration: 0.8, ease: "easeInOut" }}></motion.div>
             <motion.div
                 className="fixed top-0 bottom-0 right-full w-screen h-screen z-20 bg-light"
-                initial={{ x: "100%", width: "100%" }}
+                initial={initialLoad ? false : { x: "100%", width: "100%" }}
                 animate={{ x: "0%", width: "0%" }}
                 transition={{
                     delay: 0.2,
@@ -21,7 +28,7 @@ const TransitionEffect = () => {
                 }}></motion.div>
             <motion.div
                 className="fixed top-0 bottom-0 right-full w-screen h-screen z-10 bg-dark"
-                initial={{ x: "100%", width: "100%" }}
+                initial={initialLoad ? false : { x: "100%", width: "100%" }}
                 animate={{ x: "0%", width: "0%" }}
                 transition={{
                     delay: 0.4,
