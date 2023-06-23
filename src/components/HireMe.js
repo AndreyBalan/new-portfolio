f5db7bce1a75bcdc17c4237cbae18e96256e9f2d
Andrii Balan <balan.andrey.93@gmail.com>
2023-06-23 13:03:19 +0300

    Update HireMe.js

--- src/components/HireMe.js
+++ src/components/HireMe.js
@@ -12,7 +12,7 @@ const HireMe = () => {
                 <Link
                     href="mailto:balan.andrey.93@gmail.com"
                     className="flex items-center justify-center absolute bg-dark text-light
-            shadow-md border border-solid border-dark w-20 h-20 rounded-full font-semibold hover:bg-light hover:text-dark dark:bg-light dark:text-dark hover:dark:bg-dark hover:dark:text-light hover:dark:border-light transition-all duration-300 ease-out md:w-12 md:text-[10px] md:h-12 z-20">
+            shadow-md border border-solid border-dark w-20 h-20 rounded-full font-semibold hover:bg-light hover:text-dark dark:bg-light dark:text-dark hover:dark:bg-dark hover:dark:text-light hover:dark:border-light transition-all duration-300 ease-out md:w-12 md:text-[10px] md:h-12 z-11">
                     Hire Me
                 </Link>
             </div>
