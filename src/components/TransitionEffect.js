e937b966be1b06504a149cf11489b8ea9e255fcd
Andrii Balan <balan.andrey.93@gmail.com>
2023-06-23 13:03:41 +0300

    Update TransitionEffect.js

--- src/components/TransitionEffect.js
+++ src/components/TransitionEffect.js
@@ -20,7 +20,7 @@ const TransitionEffect = () => {
                     ease: "easeInOut",
                 }}></motion.div>
             <motion.div
-                className="fixed top-0 bottom-0 right-full w-screen h-screen z-10 bg-dark"
+                className="fixed top-0 bottom-0 right-full w-screen h-screen z-12 bg-dark"
                 initial={{ x: "100%", width: "100%" }}
                 animate={{ x: "0%", width: "0%" }}
                 transition={{
